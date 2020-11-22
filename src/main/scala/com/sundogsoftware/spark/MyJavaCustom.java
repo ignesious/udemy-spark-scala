02e874a8df7f3cac509138c6cf794afb00fc7c16
MOHAMMED MOHIDEEN <mohammedtce@gmail.com>
2020-11-22 19:49:50 +0530

    Good one

--- src/main/scala/com/sundogsoftware/spark/MyJavaCustom.java
+++ src/main/scala/com/sundogsoftware/spark/MyJavaCustom.java
@@ -1,20 +1,23 @@
 package com.sundogsoftware.spark;
 
+import java.math.BigInteger;
+
 public class MyJavaCustom {
 
-    public static void main(String args[]){
-        int i = 10;
-        if(1>3){
-            System.out.println("Gomma Punda");
-        }
-        else
+    public static void main(String args[])throws Exception{
+
+        BigInteger first =BigInteger.ZERO;
+        BigInteger second = BigInteger.ONE;
+        Long count =2L;
+        while(count <= 2000)
         {
-            System.out.println("Gotha");
+            BigInteger third = first.add(second);
+            System.out.println("Count: "+count+"\t"+" "+third);
+            first = second;
+            second = third;
+            count++;
+           // Thread.sleep(500);
         }
-
-        final String scalaVal = "mohammed";
-
-
 
     }
 }
